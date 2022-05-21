19bef7dfc5309da7b769138fd375fa964f5afc06
Austin Vickers <avickrs@gmail.com>
2022-05-21 15:37:54 -0500

    setup project and solution

---
+++ AvatarController/AvatarController/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AvatarController
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exit on Ctrl+C
+            Console.CancelKeyPress += (object? sender, ConsoleCancelEventArgs e) =>
+            {
+                e.Cancel = true;
+                Exit();
+            };
+        }
+
+        private static void Exit()
+        {
+            Console.WriteLine("Exiting...");
+        }
+    }
+}
