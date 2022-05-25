af4db53b49fd6fdf017657b7db8a4ccfb642680f
Austin Vickers <avickrs@gmail.com>
2022-05-24 19:27:53 -0500

    get extensions - work on initializing session

--- AvatarController/HapticsModule/ControllerHaptics.cs
+++ AvatarController/HapticsModule/ControllerHaptics.cs
@@ -10,6 +10,7 @@
         private XR _xrApi;
         private Instance _xrInstance;
         private Session _session;
+        private ActionSet _actionSet;
         private bool _isInitialized = false;
 
         public ControllerHaptics()
@@ -22,32 +23,76 @@
         {
             if (_xrApi != null)
             {
-                ApplicationInfo appInfo = new ApplicationInfo() { ApplicationVersion = 1, ApiVersion = XR_MAKE_VERSION(1, 0, 22) };
+                //Get number of extensions
+                UInt32 extensionCount = 0;
+                _xrApi.EnumerateInstanceExtensionProperties("", 0, &extensionCount, null);
+
+                //Get extensions
+                ExtensionProperties[] extensionProperties = new ExtensionProperties[extensionCount];
+                for (int i = 0; i < extensionCount; i++)
+                {
+                    extensionProperties[i] = new ExtensionProperties() { Type = StructureType.TypeExtensionProperties };
+                }
+                var result = _xrApi.EnumerateInstanceExtensionProperties("", extensionCount, &extensionCount, extensionProperties);
+                if(result == Result.Success && extensionProperties != null)
+                {
+                    foreach (var extensionProperty in extensionProperties)
+                    {
+                        string extensionname = Encoding.UTF8.GetString(extensionProperty.ExtensionName, 128);
+                        Console.WriteLine($"Found Extension - {extensionname} : {extensionProperty.ExtensionVersion}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Could not retrieve extension properties! Result: {result}");
+                    return _isInitialized;
+                }
+
+                //Create AppInfo
+                ApplicationInfo appInfo = new ApplicationInfo() { ApplicationVersion = 1, ApiVersion = XR_MAKE_VERSION(1, 0, 22) }; //TODO: We should actually get the version from the openxr_loader.dll if possible
                 Encoding.UTF8.GetBytes("AppName", new Span<byte>(appInfo.ApplicationName, 128));
 
+                //Initialize XR instance
                 InstanceCreateInfo instanceInfo = new InstanceCreateInfo()
                 {
                     Type = StructureType.TypeInstanceCreateInfo,
                     ApplicationInfo = appInfo
                 };
-
-                var result = _xrApi.CreateInstance(instanceInfo, ref _xrInstance);
+                result = _xrApi.CreateInstance(instanceInfo, ref _xrInstance);
                 if (result != Result.Success)
                 {
                     Console.WriteLine($"XrInstance failed to initialize! Result: {result}");
                     return _isInitialized;
                 }
 
-                //Initialize XR session
-                SessionCreateInfo sessionInfo = new SessionCreateInfo();
-                if (_xrApi.CreateSession(_xrInstance, sessionInfo, ref _session) != Result.Success)
+                //Get XR System
+                ulong systemId = 0;
+                SystemGetInfo systemInfo = new SystemGetInfo() { Type = StructureType.TypeSystemGetInfo, FormFactor = FormFactor.HeadMountedDisplay };
+                result = _xrApi.GetSystem(_xrInstance, systemInfo, ref systemId);
+                if(result != Result.Success)
                 {
-                    Console.WriteLine("XrSession failed to initialize!");
+                    Console.WriteLine($"Failed to get XR system! Result: {result}");
+                    return _isInitialized;
+                }
+
+                //Initialize XR session
+                SessionCreateInfo sessionInfo = new SessionCreateInfo() { Type = StructureType.TypeSessionCreateInfo, SystemId = systemId };      
+                result = _xrApi.CreateSession(_xrInstance, sessionInfo, ref _session);
+                if (result != Result.Success)
+                {
+                    Console.WriteLine($"XrSession failed to initialize! Result: {result}");
                     return _isInitialized;
                 }
 
                 //Create an action set
-                ActionSetCreateInfo actionSetInfo = new ActionSetCreateInfo();
+                ActionSetCreateInfo actionSetInfo = new ActionSetCreateInfo() { Type = StructureType.TypeActionSetCreateInfo, Priority = 0 };
+                Encoding.UTF8.GetBytes("ControllerHapticsActionSet", new Span<byte>(actionSetInfo.ActionSetName, 128));
+                result = _xrApi.CreateActionSet(_xrInstance, actionSetInfo, ref _actionSet);
+                if(result != Result.Success)
+                {
+                    Console.WriteLine($"Could not create action set. Result: {result}");
+                    return _isInitialized;
+                }
 
                 //Create an output haptic action
 
